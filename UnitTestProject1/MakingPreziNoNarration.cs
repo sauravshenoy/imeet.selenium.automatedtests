38d85adaf13b86ade05f560039869ab673c9450f
sauravshenoy <saurav@looplogic.com>
2016-02-22 12:54:00 -0700

    Initial Commit

---
+++ UnitTestProject1/MakingPreziNoNarration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
+using System.Linq;
+using System.Runtime;
+using Toolkit.Core.Utils;
+
+namespace Dude
+{
+    [TestClass]
+    public class FirstSeleniumTesting
+    {
+        IWebDriver driver;
+        IWebElement prezititle;
+        IWebElement upload;
+
+        [TestInitialize]
+        public void TestSetup()
+       {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://goo.gl/aSY89E");
+            Thread.Sleep(2000);
+            driver.FindElement(By.CssSelector("body > div.container > div > form > button")).Click();
+            Thread.Sleep(2000);
+            driver.Navigate().GoToUrl("http://sales.imeet.com/my-channel/Presentations/New");
+            Thread.Sleep(2000);
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void NoNarrationPreziUpload()
+        {
+            driver.FindElement(By.CssSelector("#upload-slides-container")).Click();
+            Thread.Sleep(3000);
+            prezititle = driver.FindElement(By.CssSelector("#presoTitle"));
+            prezititle.Click();
+            prezititle.SendKeys("My Computer Made This!!");
+            Thread.Sleep(3000);
+
+            //from here there are two possible paths
+
+            driver.FindElement(By.CssSelector("#slide-upload > div > form > div:nth-child(4) > label")).Click();
+            driver.FindElement(By.CssSelector("#slide-upload-container > p > a")).SendKeys("C: \\Users\\Saurav\\Downloads\\GGPrezi.pptx"); 
+            Thread.Sleep(5000);
+        }
+        [TestMethod]
+        public void VideoUpload()
+        {
+            driver.FindElement(By.CssSelector("#tool-selection > div:nth-child(2) > div:nth-child(2) > div > h3")).Click();
+            Thread.Sleep(2000);
+            prezititle= driver.FindElement(By.CssSelector("# presoTitle"));
+            prezititle.Click();
+            prezititle.SendKeys("ComputerVidUpload");
+
+        }
+        [TestMethod]
+        public void PreziUploadWithNarration()
+        {
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
+namespace Dude
+{
+    [TestClass]
+    public class Yo
+    {
+        IWebDriver driver;
+        IWebElement ss9;
+        IWebElement omg;
+        IWebElement upload;
+        IWebElement qq;
+        IWebElement passforemail;
+        string url = "https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1#identifierm";
+        [TestInitialize]
+        public void TestSetup()
+        {
+
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+        }
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void GoogleSignIn()
+        {
+
+            ss9 = driver.FindElement(By.CssSelector("#Email"));
+            ss9.Click();
+            ss9.SendKeys("saurav@looplogic.com");
+            driver.FindElement(By.CssSelector("#next")).Click();
+            Thread.Sleep(9000);
+            passforemail = driver.FindElement(By.CssSelector("#user_email"));
+            passforemail.Click();
+            passforemail.SendKeys("saurav@looplogic.com");
+            passforemail = driver.FindElement(By.CssSelector("#user_password"));
+            passforemail.Click();
+            passforemail.SendKeys("Ilovecollege1");
+            driver.FindElement(By.CssSelector("#user_submit")).Click();
+            Thread.Sleep(5000);       
+        }
+        [TestMethod]
+        public void MakingPrezi()
+        {
+            driver.Navigate().GoToUrl("http://goo.gl/aSY89E");
+            Thread.Sleep(5000);
+            driver.FindElement(By.CssSelector("body > div.container > div > form > button")).Click();
+            Thread.Sleep(5000);
+           driver.Navigate().GoToUrl("http://sales.imeet.com/my-channel/Presentations/New");
+            Thread.Sleep(8000);
+            driver.FindElement(By.CssSelector("#upload-slides-container")).Click();
+            Thread.Sleep(5000);
+            omg = driver.FindElement(By.CssSelector("#presoTitle"));
+            omg.Click();
+            omg.SendKeys("My Computer Made This!!");
+            Thread.Sleep(7000);
+            driver.FindElement(By.CssSelector("#slide-upload > div > form > div:nth-child(4) > label")).Click();
+            upload = driver.FindElement(By.CssSelector("#slide-upload-container > p > a"));
+            upload.Click();
+            upload.SendKeys("C: /Users/Saurav/Downloads/Gauss Gun Presentation.pptx");
+            
+
+        }
+        [TestMethod]
+        public void Amazon()
+        {
+            driver.Navigate().GoToUrl("http://amazon.com");
+            qq = driver.FindElement(By.CssSelector("#twotabsearchtextbox"));
+            qq.Click();
+            qq.SendKeys("Coding is Fun!");
+            Thread.Sleep(1000);
+            driver.FindElement(By.CssSelector("#nav-search > form > div.nav-right > div > input")).Click();
+
+
+        }
+    }
+}
